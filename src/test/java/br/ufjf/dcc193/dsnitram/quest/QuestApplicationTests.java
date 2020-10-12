02de8754293608f4d9212b0605302780a25b8ac4
dsnitram <diegonwb@gmail.com>
2020-10-12 20:33:35 -0300

    Cria um projeto spring boot vazio

---
+++ src/test/java/br/ufjf/dcc193/dsnitram/quest/QuestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.dsnitram.quest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
